f296ad8c4131542370b53d3512579bb83cd28447
Nolit <yaboonjapan@gmail.com>
2020-06-06 22:01:46 +0900

    持ち駒の作成

---
+++ src/main/kotlin/net/nolit/japanesechess/domain/entity/PieceInHand.kt
@@ -0,0 +1,10 @@
+package net.nolit.japanesechess.domain.entity
+
+import net.nolit.japanesechess.domain.entity.piece.Piece
+import net.nolit.japanesechess.domain.value.Position
+
+class PieceInHand(private val createPiece: (position: Position) -> Piece) {
+    fun drop(position: Position): Piece {
+        return createPiece(position)
+    }
+}
