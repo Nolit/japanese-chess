b7a4fd88fbb0b1758810da41af19054a1f9481ac
Nolit <yaboonjapan@gmail.com>
2020-06-06 22:01:25 +0900

    と金の作成

---
+++ src/main/kotlin/net/nolit/japanesechess/domain/entity/piece/PromotedPawn.kt
@@ -0,0 +1,45 @@
+package net.nolit.japanesechess.domain.entity.piece
+
+import net.nolit.japanesechess.domain.entity.PieceInHand
+import net.nolit.japanesechess.domain.value.Position
+
+class PromotedPawn(private val position: Position, private val isBlack: Boolean): Piece(position, isBlack) {
+    override fun listPositionAvailableToMove(): List<Position> {
+        return when(isBlack) {
+            //TODO: 盤外チェックをどうするか
+            true -> listOf(
+                    if(position.canMoveToFront(1)) position.toFront(1) else null,
+                    if(position.toFront(1).canMoveToRight(1)) position.toFront(1).toRight(1) else null,
+                    if(position.toFront(1).canMoveToLeft(1)) position.toFront(1).toLeft(1) else null,
+                    if(position.canMoveToRight(1)) position.toRight(1) else null,
+                    if(position.canMoveToLeft(1)) position.toLeft(1) else null,
+                    if(position.canMoveToBack(1)) position.toBack(1) else null
+            )
+            false -> listOf(
+                    if(position.canMoveToBack(1)) position.toBack(1) else null,
+                    if(position.toBack(1).canMoveToRight(1)) position.toBack(1).toRight(1) else null,
+                    if(position.toBack(1).canMoveToLeft(1)) position.toBack(1).toLeft(1) else null,
+                    if(position.canMoveToRight(1)) position.toRight(1) else null,
+                    if(position.canMoveToLeft(1)) position.toLeft(1) else null,
+                    if(position.canMoveToFront(1)) position.toFront(1) else null
+
+            )
+        }.filterNotNull()
+    }
+
+    override fun setPosition(position: Position): Piece {
+        return Pawn(position, isBlack)
+    }
+
+    override fun mustPromote(toPosition: Position): Boolean {
+        return false
+    }
+
+    override fun promote(): Piece {
+        throw IllegalCallerException("promoted pawn cannot promote")
+    }
+
+    override fun toPieceInHand(): PieceInHand {
+        return PieceInHand { position -> Pawn(position, !isBlack) }
+    }
+}
