1b4ace0d411bacfb0880db07790868546970e3c1
Nolit <yaboonjapan@gmail.com>
2020-08-16 07:50:03 +0900

    ト金の移動範囲の修正

--- src/main/kotlin/net/nolit/japanesechess/domain/entity/piece/PromotedPawn.kt
+++ src/main/kotlin/net/nolit/japanesechess/domain/entity/piece/PromotedPawn.kt
@@ -5,29 +5,33 @@ import net.nolit.japanesechess.domain.value.Position
 
 class PromotedPawn(override val position: Position, override val isBlack: Boolean): Piece(position, isBlack) {
     override fun listPositionAvailableToMove(): List<Position> {
+        val canMoveToFront = position.canMoveToFront(1)
+        val canMoveToRight = position.canMoveToRight(1)
+        val canMoveToLeft = position.canMoveToLeft(1)
+        val canMoveToBack = position.canMoveToBack(1)
         return when(isBlack) {
             true -> listOf(
-                    if(position.canMoveToFront(1)) position.toFront(1) else null,
-                    if(position.toFront(1).canMoveToRight(1)) position.toFront(1).toRight(1) else null,
-                    if(position.toFront(1).canMoveToLeft(1)) position.toFront(1).toLeft(1) else null,
-                    if(position.canMoveToRight(1)) position.toRight(1) else null,
-                    if(position.canMoveToLeft(1)) position.toLeft(1) else null,
-                    if(position.canMoveToBack(1)) position.toBack(1) else null
+                    if(canMoveToFront) position.toFront(1) else null,
+                    if(canMoveToFront && canMoveToRight) position.toFront(1).toRight(1) else null,
+                    if(canMoveToFront && canMoveToLeft) position.toFront(1).toLeft(1) else null,
+                    if(canMoveToRight) position.toRight(1) else null,
+                    if(canMoveToLeft) position.toLeft(1) else null,
+                    if(canMoveToBack) position.toBack(1) else null
             )
             false -> listOf(
-                    if(position.canMoveToBack(1)) position.toBack(1) else null,
-                    if(position.toBack(1).canMoveToRight(1)) position.toBack(1).toRight(1) else null,
-                    if(position.toBack(1).canMoveToLeft(1)) position.toBack(1).toLeft(1) else null,
-                    if(position.canMoveToRight(1)) position.toRight(1) else null,
-                    if(position.canMoveToLeft(1)) position.toLeft(1) else null,
-                    if(position.canMoveToFront(1)) position.toFront(1) else null
+                    if(canMoveToBack) position.toBack(1) else null,
+                    if(canMoveToBack && canMoveToRight) position.toBack(1).toRight(1) else null,
+                    if(canMoveToBack && canMoveToLeft) position.toBack(1).toLeft(1) else null,
+                    if(canMoveToRight) position.toRight(1) else null,
+                    if(canMoveToLeft) position.toLeft(1) else null,
+                    if(canMoveToFront) position.toFront(1) else null
 
             )
         }.filterNotNull()
     }
 
     override fun setPosition(position: Position): Piece {
-        return Pawn(position, isBlack)
+        return PromotedPawn(position, isBlack)
     }
 
     override fun mustPromote(toPosition: Position): Boolean {
